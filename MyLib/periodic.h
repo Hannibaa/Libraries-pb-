fdfcd2703b6d071440348935bc93d9fcf5218716
Kadda Aoues <108931156+Hannibaa@users.noreply.github.com>
2023-10-23 11:03:12 +0300

    Delete MyLib directory

--- MyLib/periodic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-/*
-       Check the function if it s periodic or not periodic 
-       arithmetic type periodic radian, degree, grad...
-       creat a periodic data string, ... vector ...
-       linked list periodic.
-*/
